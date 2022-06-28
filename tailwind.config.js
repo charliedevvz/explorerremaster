aa89309c3e8460816eb6e7b2d29bf4122d80e64d
Manu <charlidevvz@gmail.com>
2022-06-28 18:58:25 -0400

    done

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  content: [
+    "index.html",
+    "warrior.html",
+    "magician.html",
+    "bowman.html",
+    "thief.html",
+    "pirate.html",
+    "corsair.html",
+    "bucc.html",
+    "cannon.html",
+    "hero.html",
+    "paladin.html",
+    "drk.html",
+    "bowmaster.html",
+    "mm.html",
+    "nl.html",
+    "shad.html",
+    "db.html",
+    "il.html",
+    "fp.html",
+    "bishop.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2a606f",
+        secondary: "#323b59",
+        terciary: "#3c8396",
+        mainbg: "#1a3045",
+        blackish: "#0b122c",
+        blacke: "#000000",
+      },
+      backgroundImage: {
+        perion: "url('/img/perion.png')",
+      },
+    },
+    fontFamily: {
+      kanit: ["Kanit"],
+    },
+  },
+  plugins: [],
+};
